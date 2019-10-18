02030071877d085601e41f17dc3e8eb44174ce59
杜政伟 <duzhengwei@jiandan100.cn>
2019-10-18 11:46:11 +0800

    start work

---
+++ src/component/dquill.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div ref="dquill"></div>
+    </div>
+</template>
+
+<script>
+import Quill from 'quill'
+
+export default {
+    name: 'd-quill',
+    data(){
+
+    },
+    mounted() {
+      this.initialize()
+    },
+    beforeDestroy() {
+      this.quill = null
+      delete this.quill
+    },
+    methods:{
+        initialize(){
+            this.quill = new Quill(this.$refs.dquill)
+        }
+    }
+}
+</script>
